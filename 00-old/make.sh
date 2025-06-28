f16a502ca82d635bc7cf56687c4eb03aba0899c6
tom <1259233520@qq.com>
2025-06-28 10:58:12 +0800

    add printf and Sort out the contents list.

--- 00-old/make.sh
+++ 00-old/make.sh
@@ -16,4 +16,4 @@ dd if=/dev/zero of=fakespl.bin     count=1 bs=1
 sudo ./RKLoaderTools/boot_merger RKLoader.ini
 
 
-/home/tom/project/littleSystem/build/uboot/build/tools/mkimage -n rk3588 -T rksd -d /home/tom/project/littleSystem/build/rkbin/bin/rk35/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin:tpl.bin idbloader.img
+../tools/mkimage -n rk3588 -T rksd -d ../tools/rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin:tpl.bin idbloader.img
---
+++ 02-print/src/main.c
@@ -0,0 +1,8 @@
+// main.c
+
+#include "printf.h"
+
+void main() {
+    printf("Booting...\n");
+    printf("Hello %s, num = %d, hex = %x, char = %c\n", "world", 123, 0xABCD, 'Z');
+}
